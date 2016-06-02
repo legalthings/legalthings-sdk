3071ae791c69a9cafa6db4e7e7b1960d3df30264
Zaky Huraibi <superkytoz@gmail.com>
2016-06-02 10:49:29 +0200

    Added getSessionAPI() method

--- src/Auth.php
+++ src/Auth.php
@@ -75,4 +75,24 @@ class Auth
 
         return $obj;
     }
+    
+    public function getSessionAPI($sessionId) {
+        $client = new Client([]);
+        try {
+            $response = $client->get('http://firm24.docarama.com/service/iam/sessions/' . $sessionId, [
+                'headers' => [
+                    'Content-Type' => 'application/json'
+                ]
+            ]);
+        } catch (RequestException $e) {
+            echo 'Uh oh! ' . $e->getMessage();
+                if ($e->hasResponse()) {
+                    echo $e->getResponse();
+                }
+        }
+        $data = $response->getBody();
+        $obj = json_decode($data);
+
+        return $obj;
+    }
 }
